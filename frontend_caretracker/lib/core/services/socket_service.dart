c436747847f8160dd24854a0faf0eba694a61753
hund555 <allanholmiversen@gmail.com>
2026-06-02 09:20:29 +0200

    Changed ip to localhost for now

--- frontend_caretracker/lib/core/services/socket_service.dart
+++ frontend_caretracker/lib/core/services/socket_service.dart
@@ -9,7 +9,7 @@ class SocketService
   void connect() 
   {
     _channel = WebSocketChannel.connect(
-      Uri.parse('ws://10.176.69.104:5000'),
+      Uri.parse('ws://localhost:5000'),
     );
 
     // Identify as dashboard after connecting
