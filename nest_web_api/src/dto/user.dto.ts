024add0944ff8cdf6cd9b430692f67b42de1826d
hund555 <allanholmiversen@gmail.com>
2026-06-04 12:16:35 +0200

    changed user entity to save passwordhash and passwordsalt, changed login, create user and update user to use bcrypt, added unit test for login

--- nest_web_api/src/dto/user.dto.ts
+++ nest_web_api/src/dto/user.dto.ts
@@ -1,18 +1,21 @@
 export class CreateUserDto {
   Email: string;
   Name: string;
-  Password: string;
-  Hash?: string;
+  Password: string; // plain text input, hashed in service
   Role_ID: number;
 }
 
 export class UpdateUserDto {
   Email?: string;
   Name?: string;
-  Password?: string;
-  Hash?: string;
+  Password?: string; // plain text input, rehashed in service if provided
 }
 
 export class CreateRoleDto {
   Role: string;
+}
+
+export class LoginDto {
+  Email: string;
+  Password: string;
 }
